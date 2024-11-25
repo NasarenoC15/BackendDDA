2dcbe181d476be44389d0e441d69123ca83b1541
Nasareno Calistro <71232875+NasarenoC15@users.noreply.github.com>
2024-11-25 16:14:25 -0300

    archivo base

---
+++ backend_obg_2/src/main/java/com/backend_obg_2/backend_obg_2/BackendObg2Application.java
@@ -0,0 +1,13 @@
+package com.backend_obg_2.backend_obg_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendObg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendObg2Application.class, args);
+	}
+
+}
---
+++ backend_obg_2/src/test/java/com/backend_obg_2/backend_obg_2/BackendObg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_obg_2.backend_obg_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendObg2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
