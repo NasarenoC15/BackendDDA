b0d8a676d7d719467214b2464a08cb3abb8acc7e
CesarDP6 <cesardeleonn@hotmail.com>
2024-11-25 16:54:01 -0300

    venta

--- backend_obg_2/src/main/java/com/backend_obg_2/backend_obg_2/Entity/Venta.java
+++ backend_obg_2/src/main/java/com/backend_obg_2/backend_obg_2/Entity/Venta.java
@@ -1,5 +1,7 @@
 package com.backend_obg_2.backend_obg_2.Entity;
 
+import java.util.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
