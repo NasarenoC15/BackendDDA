6182372537f1e1ba04cd81c8e0b6767523601ee0
Agustina-Pouy <agustina.pouy@hotmail.com>
2024-11-25 17:58:07 -0300

    hgfdfgh

--- backend_obg_2/src/main/java/com/backend_obg_2/backend_obg_2/Service/CategoriaServiceImpl.java
+++ backend_obg_2/src/main/java/com/backend_obg_2/backend_obg_2/Service/CategoriaServiceImpl.java
@@ -1,5 +1,46 @@
 package com.backend_obg_2.backend_obg_2.Service;
 
-public class CategoriaServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.config.Task;
 
+import com.backend_obg_2.backend_obg_2.Entity.Categoria;
+import com.backend_obg_2.backend_obg_2.Repository.CategoriaRepository;
+
+public class CategoriaServiceImpl implements CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria agregarCategoria(Categoria c){
+        return categoriaRepository.save(c);
+    }
+
+    public boolean eliminarCategoria(int id){
+        if(categoriaRepository.existsById(id)){
+            categoriaRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public Categoria modificarCategoria(Categoria c){
+        if(categoriaRepository.existsById(c.getId())){
+            return categoriaRepository.save(c);
+        }
+        return null;
+    }
+
+    public Categoria conseguir(int id){
+        Optional<Categoria> opcional = categoriaRepository.findById(id);
+        
+        if(opcional.isPresent()){
+            return opcional.get();
+        }
+
+        return null;
+    }
+
+    public List<Categoria> listar(){
+        return categoriaRepository.findAll();
+    }
 }
