57326b77402fbc3d28d128ef920ce06bd5535d93
CesarDP6 <cesardeleonn@hotmail.com>
2024-11-27 20:32:13 -0300

    rest

--- backend_obg_2/src/main/java/com/backend_obg_2/backend_obg_2/Service/VideoJuegoServiceImpl.java
+++ backend_obg_2/src/main/java/com/backend_obg_2/backend_obg_2/Service/VideoJuegoServiceImpl.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.config.Task;
+
 import org.springframework.stereotype.Service;
 
 import com.backend_obg_2.backend_obg_2.Entity.videoJuego;
